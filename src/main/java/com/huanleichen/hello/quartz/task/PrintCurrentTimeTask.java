caa7f1798825c6ab5981691d21297e0833fd4279
LikeDrinkingWater_glb <LikeDrinkingWater_glb@chl.com>
2019-05-08 10:37:40 +0800

    hello quartz

---
+++ src/main/java/com/huanleichen/hello/quartz/task/PrintCurrentTimeTask.java
@@ -0,0 +1,15 @@
+package com.huanleichen.hello.quartz.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class PrintCurrentTimeTask {
+    @Scheduled(cron = "0/10 * * * * ? ")
+    public void printCurrentTimeTask() {
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+    }
+}
